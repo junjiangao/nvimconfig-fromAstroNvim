e3977ed46f70efb9739bc4b0a6601aacba5ceb74
junjiangao <junjiangao@gmail.com>
2024-04-24 14:27:43 +0800

    init my custom config

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "astrodark",
+    colorscheme = "onedark_vivid",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
@@ -19,6 +19,36 @@ return {
       },
       astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
+      },
+    },
+    status = {
+      -- Configure attributes of components defined in the `status` API. Check the AstroNvim documentation for a complete list of color names, this applies to colors that have `_fg` and/or `_bg` names with the suffix removed (ex. `git_branch_fg` as attributes from `git_branch`).
+      attributes = {
+        git_branch = { bold = true },
+      },
+      -- Configure colors of components defined in the `status` API. Check the AstroNvim documentation for a complete list of color names.
+      colors = {
+        git_branch_fg = "#ABCDEF",
+      },
+      -- Configure which icons that are highlighted based on context
+      icon_highlights = {
+        -- enable or disable breadcrumb icon highlighting
+        breadcrumbs = false,
+        -- Enable or disable the highlighting of filetype icons both in the statusline and tabline
+        file_icon = {
+          tabline = function(self) return self.is_active or self.is_visible end,
+          statusline = true,
+        },
+      },
+      -- Configure characters used as separators for various elements
+      separators = {
+        none = { "", "" },
+        left = { "", " " },
+        right = { " ", "" },
+        center = { " ", " " },
+        tab = { "", "" },
+        breadcrumbs = "  ",
+        path = "  ",
       },
     },
     -- Icons can be configured throughout the interface
