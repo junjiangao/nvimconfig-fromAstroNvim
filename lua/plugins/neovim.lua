29edb8cbdfe0d0349d7ca8fabd364cc85d8399a5
gaojunjian <gaojunjian@uniontech.com>
2024-08-26 11:52:26 +0800

    disable perl check

---
+++ lua/plugins/neovim.lua
@@ -0,0 +1,12 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      g = { -- configure vim.g variables
+        -- configure perl
+        loaded_perl_provider = 0,
+      },
+    },
+  },
+}
