0313c8cc50f50bfa34ef771706be210e3e8771f3
xyzhuanjian <junjiangao@live.com>
2024-10-14 22:22:07 +0800

    fix provider_disable.lua

--- lua/plugins/provider_disable.lua
+++ lua/plugins/provider_disable.lua
@@ -1,17 +1,32 @@
 local home_dir = os.getenv "HOME"
-if home_dir ~= "/root" then return {} end
-
-return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    options = {
-      g = {
-        -- configure vim.g variables
-        loaded_perl_provider = 0,
-        loaded_node_provider = 0,
-        loaded_ruby_provider = 0,
+if home_dir ~= "/root" then
+  return {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        g = {
+          -- configure vim.g variables
+          loaded_perl_provider = 0,
+          loaded_node_provider = 1,
+          loaded_ruby_provider = 0,
+        },
       },
     },
-  },
-}
+  }
+else
+  return {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        g = {
+          -- configure vim.g variables
+          loaded_perl_provider = 0,
+          loaded_node_provider = 0,
+          loaded_ruby_provider = 0,
+        },
+      },
+    },
+  }
+end
