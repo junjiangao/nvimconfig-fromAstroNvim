9d370d81aa8cfad4e44c0e47a9c64073c41e0325
xyzhuanjian <junjiangao@live.com>
2024-10-14 22:16:22 +0800

    different root and normal user

---
+++ lua/plugins/provider_disable.lua
@@ -0,0 +1,17 @@
+local home_dir = os.getenv "HOME"
+if home_dir ~= "/root" then return {} end
+
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      g = {
+        -- configure vim.g variables
+        loaded_perl_provider = 0,
+        loaded_node_provider = 0,
+        loaded_ruby_provider = 0,
+      },
+    },
+  },
+}
