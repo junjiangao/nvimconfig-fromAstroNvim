3be7260b1b99485040c28dc03c778d3a1d02325f
gaojunjian <gaojunjian@uniontech.com>
2025-01-22 14:03:13 +0800

    修复配置问题

--- lua/plugins/custom_user.lua
+++ lua/plugins/custom_user.lua
@@ -8,13 +8,14 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    g = {
-      -- 配置 vim.g 变量
-      loaded_perl_provider = 0, -- 禁用 Perl 支持
-      loaded_ruby_provider = 0, -- 禁用 Ruby 支持
-      loaded_node_provider = node_provider, -- 根据条件设置 Node 支持
-    },
     options = {
+      g = {
+        -- 配置 vim.g 变量
+        loaded_perl_provider = 0, -- 禁用 Perl 支持
+        loaded_ruby_provider = 0, -- 禁用 Ruby 支持
+        loaded_node_provider = node_provider, -- 根据条件设置 Node 支持
+      },
+
       opt = {
         fileencodings = "ucs-bom,utf-8,gb18030,gbk,gb2312,cp936,default,big5,euc-jp,euc-kr,latin1",
       },
