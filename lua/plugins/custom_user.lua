d33016c4df631e32103820bacf3375981276554a
gaojunjian <gaojunjian@uniontech.com>
2025-01-22 13:45:31 +0800

    调整自定义配置

---
+++ lua/plugins/custom_user.lua
@@ -0,0 +1,23 @@
+local home_dir = os.getenv "HOME"
+
+-- 根据条件设置 loaded_node_provider
+local node_provider = home_dir == "/root" and 0 or 1
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    g = {
+      -- 配置 vim.g 变量
+      loaded_perl_provider = 0, -- 禁用 Perl 支持
+      loaded_ruby_provider = 0, -- 禁用 Ruby 支持
+      loaded_node_provider = node_provider, -- 根据条件设置 Node 支持
+    },
+    options = {
+      opt = {
+        fileencodings = "ucs-bom,utf-8,gb18030,gbk,gb2312,cp936,default,big5,euc-jp,euc-kr,latin1",
+      },
+    },
+  },
+}
